d32c987e970d416eef55b938746e79107ff08c66
JohaoG10 <johao.gavilanes@udla.edu.ec>
2024-10-28 08:09:04 -0500

    Agregar archivos de proyecto.

---
+++ GAVILANESJOHAO-EXA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GAVILANESJOHAO_EXA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
